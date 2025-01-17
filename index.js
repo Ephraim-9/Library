d8d020ee69edc796a517758e3c96586416f2cdc3
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-01-17 15:44:56 +0500

    add books

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ addBookToLibrary('Crime and Punishment', 'Fyodor Dostoevsky', 430, true, 'https:
 addBookToLibrary('Thus Spoke Zarathustra', 'Friedrich Nietzsche', 368, false, 'https://i.pinimg.com/236x/47/04/aa/4704aa893c20577bceb7081053c3c6e1.jpg');
 addBookToLibrary('The Metamorphosis', 'Franz Kafka', 120, false, 'https://i.pinimg.com/236x/29/17/6f/29176fb9a7e14fc536beab10b5791dba.jpg');
 
+addBookToLibrary('The Stranger', 'Albert Camus', 123, true, 'https://i.pinimg.com/236x/1e/41/ac/1e41ac43ba3556f962ca6f0baef52719.jpg');
+addBookToLibrary('The Trial', 'Franz Kafka', 256, true, 'https://i.pinimg.com/236x/31/30/d7/3130d75c76cea6d83262794a4cf88b6c.jpg');
+addBookToLibrary('The Bell Jar', 'Sylvia Plath', 244, true, 'https://i.pinimg.com/236x/2e/d8/e1/2ed8e17d003f3e572504d28aeaec132e.jpg');
+addBookToLibrary('Notes from Underground', 'Fyodor Dostoevsky', 192, true, 'https://i.pinimg.com/474x/ca/f8/c1/caf8c1a9ce7e083725cc692f5e8dc6b1.jpg');
+addBookToLibrary('The Plague', 'Albert Camus', 308, false, 'https://i.pinimg.com/236x/b8/32/51/b83251b2cde102692b6c457af4c98af6.jpg');
+addBookToLibrary('The Brothers Karamazov', 'Fyodor Dostoevsky', 796, true, 'https://i.pinimg.com/474x/e0/53/6e/e0536edbe43c84903fc5c2189aca36f9.jpg');
+addBookToLibrary('The Unbearable Lightness of Being', 'Milan Kundera', 320, false, 'https://i.pinimg.com/736x/e2/9c/25/e29c254d1eb42e663651165ecea9925f.jpg');
+addBookToLibrary('The Catcher in the Rye', 'J.D. Salinger', 277, true, 'https://i.pinimg.com/236x/24/08/0e/24080eee533e92c25cb3b8e7d37284f5.jpg');
+
 console.log(myLibrary)
 
 
